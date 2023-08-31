11cd1ce2f5b3b43f3ebec2db93a2e9ccc54b24fa
toastedbreadandomelette <toastedbreadandomelette@gmail.com>
2023-08-31 22:42:08 +0530

    Remove vec

--- linalg/src/matmul/matmul_normal.rs
+++ linalg/src/matmul/matmul_normal.rs
@@ -22,26 +22,3 @@ pub fn matmul_normal(
     }
     c
 }
-
-/// Multiply two matrices `a` and `b` of size
-/// `ashape (m x n)` and `bshape(n x p)`
-///
-/// Returns new matrix vector `c` of size (m x p)
-pub fn matmul_normal_vec(
-    a: &[f64],
-    b: &[f64],
-    ashape: (usize, usize),
-    bshape: (usize, usize),
-) -> Vec<f64> {
-    assert!(ashape.1 == bshape.0);
-    let (m, n, p) = (ashape.0, ashape.1, bshape.1);
-    let mut c: Vec<f64> = vec![0.0; m * p];
-    for i in 0..m {
-        for j in 0..p {
-            for k in 0..n {
-                c[i * p + j] += a[i * n + k] * b[k * p + j];
-            }
-        }
-    }
-    c
-}
